8d0f59b71ea464214bfab0b25f47b65b44e1a2c2
V0ltaroo <nendorans@gmail.com>
2023-04-27 23:17:02 +0200

    [feature]: Add mostly implemented jumping

--- data/game_runner.py
+++ data/game_runner.py
@@ -5,6 +5,7 @@ from sys import exit
 """
 - inicializacia pygame (vytvorenie okna, pridanie popisku, velkost)
 - pridanie hlavneho cyklu (while True: ...)
+- pridanie pozadia a zeme (ako surface a vyplnene farbou)
 
 """
 
@@ -13,12 +14,64 @@ from sys import exit
 pygame.init()
 clock = pygame.time.Clock()
 
+#* Colors
+BG_COLOR_SCREEN = (40, 40, 40)
+BG_COLOR_DEFAULT = (0, 143, 230)
+BG_COLOR_SURFACE = (100, 100, 100)
+COLOR_PLAYER = (200, 0, 0)
+
+
 
 #* Window settings
 [SCREEN_WIDTH, SCREEN_HEIGHT] = [800, 600]
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Moon Patrol v0.0 - FPV UMB')
 FPS = 60
+
+
+#* Surfaces - Main
+background_surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+background_surface.fill(BG_COLOR_SCREEN)
+
+ground_surface = pygame.Surface((SCREEN_WIDTH, 150))
+ground_surface.fill(BG_COLOR_SURFACE)
+
+# print(ground_surface.get_rect().top, ground_surface.get_rect().bottom)
+
+#* Player
+[PLAYER_WIDTH, PLAYER_HEIGHT] = [100, 100]
+class Vehicle:
+    def __init__(self, sizeX: int, sizeY: int, color: tuple, texture: str, surface: pygame.Surface):
+        self.w = sizeX
+        self.h = sizeY
+        self.texture = None if texture == '' else texture
+        self.color = color
+        self.surface = surface
+        self.rect = pygame.Rect(0, self.surface.get_height() - self.h - ground_surface.get_height(), self.w, self.h)
+        self.isJumping = False
+        self.jumpCount = 10
+    
+    def jump(self):
+        print(self.jumpCount, self.isJumping)
+        if not self.isJumping:
+            if self.rect.y <= SCREEN_HEIGHT - ground_surface.get_rect().bottom - PLAYER_HEIGHT - 10:
+        #         # print('Som nad zemou, POMOOOC')
+                self.rect.y += int(abs(self.jumpCount * 2) * 0.5)
+        # else:
+        #     if self.jumpCount > (self.jumpCount - self.jumpCount * 2):
+        #         self.rect.y -= int((self.jumpCount * abs(self.jumpCount)) * 0.5)
+        #         self.jumpCount -= 1
+        #         print(self.rect.y, self.jumpCount)
+        #     else:
+        #         self.jumpCount = 10
+        #         self.isJumping = False
+
+    
+    def render(self):
+        self.jump()
+        pygame.draw.rect(screen, self.color, self.rect)
+
+playerVehicle = Vehicle(PLAYER_WIDTH, PLAYER_HEIGHT, COLOR_PLAYER, '', background_surface)
 
 
 #* Fonts
@@ -34,11 +87,34 @@ while True:
         if e.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # Keyboard Action Listeners
+        if e.type == pygame.KEYDOWN:
+            print('Some key got pressed')
+            if e.key == pygame.K_SPACE and playerVehicle.isJumping is not True:
+                playerVehicle.isJumping = True
+                # playerVehicle.jumpCount = 10
+                # playerVehicle.jump()
+                break
+            else:
+                playerVehicle.isJumping = False
+                break
+            
+                
     
     # Screen Render
-    screen.fill((40, 40, 40))
+    screen.fill(BG_COLOR_SCREEN)
+    screen.blit(background_surface, (0, 0))
+    screen.blit(ground_surface, (0, SCREEN_HEIGHT - ground_surface.get_height()))
+    
+    # Objects Render
+    playerVehicle.render()
+    
+    
+    # player_object = pygame.draw.rect(screen, (COLOR_PLAYER), (0, SCREEN_HEIGHT - PLAYER_HEIGHT - ground_surface.get_height(), PLAYER_WIDTH, PLAYER_HEIGHT), 1)
     
     # Window Update
+    pygame.display.update()
+
     pygame.display.update()
     delta = clock.tick(FPS) * 0.001
     clock.tick(FPS / delta)
