fe43cfe69edf947815596294a67385154a44453f
V0ltaroo <nendorans@gmail.com>
2023-05-19 08:53:33 +0200

    [feature]: Obstacle Class Initialization

--- data/game_runner.py
+++ data/game_runner.py
@@ -42,49 +42,70 @@ ground_surface.fill(BG_COLOR_SURFACE)
 #* Player
 [PLAYER_WIDTH, PLAYER_HEIGHT] = [100, 100]
 class Vehicle:
-	def __init__(self, sizeX: int, sizeY: int, color: tuple, texture: str, surface: pygame.Surface):
-		self.w = sizeX
-		self.h = sizeY
-		self.texture = None if texture == '' else texture
-		self.color = color
-		self.surface = surface
-		self.rect = pygame.Rect(0, self.surface.get_height() - self.h - ground_surface.get_height(), self.w, self.h)
-		self.isJumping = False
-		self.jumpCount = 10
+    def __init__(self, sizeX: int, sizeY: int, color: tuple, texture: str, surface: pygame.Surface):
+        self.w = sizeX
+        self.h = sizeY
+        self.texture = None if texture == '' else texture
+        self.color = color
+        self.surface = surface
+        self.rect = pygame.Rect(0, self.surface.get_height() - self.h - ground_surface.get_height(), self.w, self.h)
+        self.isJumping = False
+        self.jumpCount = 10
 
-		self.firstJump = True
-		self.jumpSegment = 0.045
-		self.hranica = 100
-		self.zem = self.rect.y
+        self.firstJump = True
+        self.jumpSegment = 0.045
+        self.hranica = 100
+        self.zem = self.rect.y
 
-	
-	def jump(self):
-		if (self.isJumping):
-			if (self.firstJump):
-				self.rect.y -= 2 * self.jumpCount
-				self.firstJump = False
+    
+    def jump(self):
+        if (self.isJumping):
+            if (self.firstJump):
+                self.rect.y -= 2 * self.jumpCount
+                self.firstJump = False
 
-			if (self.rect.top >= self.zem):
-				print('Si na zemi')
-				self.jumpSegment = -self.jumpSegment
-				self.isJumping = False
-				self.firstJump = True
-				self.rect.y = self.zem
-				return
+            if (self.rect.top >= self.zem):
+                print('Si na zemi')
+                self.jumpSegment = -self.jumpSegment
+                self.isJumping = False
+                self.firstJump = True
+                self.rect.y = self.zem
+                return
 
-			if (self.rect.top <= self.hranica):
-				print('Hitol si hranicu')
-				self.jumpSegment = -self.jumpSegment
-			
-			if (self.rect.y <= self.zem and self.rect.y >= self.hranica):
-				rovnica = -((self.rect.y / 18) ** 2)
-				self.rect.y += rovnica * self.jumpSegment
-	
-	def render(self):
-		self.jump()
-		pygame.draw.rect(screen, self.color, self.rect)
+            if (self.rect.top <= self.hranica):
+                print('Hitol si hranicu')
+                self.jumpSegment = -self.jumpSegment
+            
+            if (self.rect.y <= self.zem and self.rect.y >= self.hranica):
+                rovnica = -((self.rect.y / 18) ** 2)
+                self.rect.y += rovnica * self.jumpSegment
+    
+    def render(self):
+        self.jump()
+        pygame.draw.rect(screen, self.color, self.rect)
 
 playerVehicle = Vehicle(PLAYER_WIDTH, PLAYER_HEIGHT, COLOR_PLAYER, '', background_surface)
+
+
+class Obstacle:
+    def __init__(self, sizeX: int, sizeY: int, color: tuple, texture: str, surface: pygame.Surface):
+        self.w = sizeX
+        self.h = sizeY
+        self.texture = None if texture == '' else texture
+        self.color = color
+        self.surface = surface
+        self.rect = pygame.Rect(0, self.surface.get_height(
+            ) - self.h - ground_surface.get_height(), self.w, self.h)
+        self.isJumping = False
+        self.jumpCount = 10
+
+        self.firstJump = True
+        self.jumpSegment = 0.045
+        self.hranica = 100
+        self.zem = self.rect.y
+    
+    def render(self):
+        pygame.draw.rect(screen, self.color, self.rect)
 
 
 #* Fonts
@@ -93,31 +114,31 @@ game_font_main = pygame.font.Font('assets/fonts/PublicPixel-z84yD.ttf', 14)
 
 #* Game Runner
 while True:
-	
-	# Event Listener
-	for e in pygame.event.get():
+    
+    # Event Listener
+    for e in pygame.event.get():
 
-		# Window Close
-		if e.type == pygame.QUIT:
-			pygame.quit()
-			exit()
-		
-		# Keyboard Action Listeners
-		if e.type == pygame.KEYDOWN:
-			print('Some key got pressed')
-			if e.key == pygame.K_SPACE and not playerVehicle.isJumping:
-				playerVehicle.isJumping = True
-	
-	# Screen Render
-	screen.fill(BG_COLOR_SCREEN)
-	screen.blit(background_surface, (0, 0))
-	screen.blit(ground_surface, (0, SCREEN_HEIGHT - ground_surface.get_height()))
-	
-	# Objects Render
-	playerVehicle.render()
-	
-	# Window Update
-	pygame.display.update()
-	pygame.display.update()
-	clock.tick(FPS)
-	
+        # Window Close
+        if e.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        
+        # Keyboard Action Listeners
+        if e.type == pygame.KEYDOWN:
+            print('Some key got pressed')
+            if e.key == pygame.K_SPACE and not playerVehicle.isJumping:
+                playerVehicle.isJumping = True
+    
+    # Screen Render
+    screen.fill(BG_COLOR_SCREEN)
+    screen.blit(background_surface, (0, 0))
+    screen.blit(ground_surface, (0, SCREEN_HEIGHT - ground_surface.get_height()))
+    
+    # Objects Render
+    playerVehicle.render()
+    
+    # Window Update
+    pygame.display.update()
+    pygame.display.update()
+    clock.tick(FPS)
+    
